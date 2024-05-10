c727ca2202753d539f879c94494ce0596975f63c
gheeeeeeee <gheoputra55@gmail.com>
2024-05-10 15:54:31 +0700

    membuat function dengan nama isEmpty menggunakan typedata boolean

--- stackusinglinkedlist_0058/stackusinglinkedlist_0058.cpp
+++ stackusinglinkedlist_0058/stackusinglinkedlist_0058.cpp
@@ -57,6 +57,11 @@ public:
             cout << endl;
         }// Return the value of the top node
     }
+
+    // isEmpty operation: Check if the stack is empty
+    bool isEmpty() {
+        return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+    }
 };
 
 
